8df8b7e3bb2a1363b49621e92dd11516cc527ea8
ormesino <pedromateus18@hotmail.com>
2024-03-24 20:37:03 -0300

    add: atividade 5 do lab 1

---
+++ lab01/ativ5.c
@@ -0,0 +1,127 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+// Constantes
+#define RANGE 20
+#define NELEMENTS 200
+#define MTHREADS 19
+
+// Vetor que será utilizado pelas threads e vetor de checagem final
+int arr[NELEMENTS], testArr[NELEMENTS];
+
+// Estrutura para auxiliar na multiplicação
+typedef struct
+{
+  int indexArr, qntyElements;
+} tArgs;
+
+// Função de inicialização do vetor
+void initArr(void)
+{
+  int temp;
+
+  // Utilização de inteiros randômicos na inicialização
+  srand(time(NULL));
+  printf("Vetor inicial: \n");
+  for (int i = 0; i < NELEMENTS; i++)
+  {
+    temp = rand() % RANGE;
+    arr[i] = temp;
+    testArr[i] = temp;
+    printf("%d ", temp);
+  }
+  printf("\n");
+}
+
+// Função de multiplicação informada para cada thread
+void *multElement(void *arg)
+{
+  tArgs args = *(tArgs *)arg;
+
+  // Multiplica desde o índice dado até a qntd de elementos da thread
+  for (int i = args.indexArr; i < args.indexArr + args.qntyElements; i++)
+    arr[i] *= arr[i];
+
+  free(arg);
+  pthread_exit(NULL);
+}
+
+// Função para checar o vetor resultado após as multiplicações
+int checkArr(void)
+{
+  for (int i = 0; i < NELEMENTS; i++)
+  {
+    if (testArr[i] != 0 && testArr[i] != arr[i] / testArr[i])
+    {
+      printf("ERRO: Checagem identificou uma multiplicação incorreta.\n");
+      exit(-3);
+    }
+  }
+  return 0;
+}
+
+int main(void)
+{
+  pthread_t tid[MTHREADS];
+  tArgs *args;
+
+  // Inicializa os vetores
+  initArr();
+
+  // Checagem entre número de elementos e número de threads
+  if (MTHREADS > NELEMENTS)
+  {
+    printf("ERRO: Informe uma quantidade de elementos maior do que a de threads.");
+    exit(1);
+  }
+
+  // Definição da quantidade de elementos por thread e o limite
+  int elemByThread = fabs(NELEMENTS / MTHREADS);
+  int limit = NELEMENTS % MTHREADS;
+
+  // Loop de criação das threads
+  for (int i = 0, index = 0; i < MTHREADS; i++)
+  {
+    args = malloc(sizeof(tArgs));
+
+    args->indexArr = index;
+    // Se estiver dentro do limite, adiciona mais um elemento
+    if (i < limit)
+      args->qntyElements = elemByThread + 1;
+    else
+      args->qntyElements = elemByThread;
+
+    // Criação da thread com o objeto como argumento
+    if (pthread_create(&tid[i], NULL, multElement, (void *)args))
+    {
+      printf("ERRO: pthread_create()\n");
+      exit(-1);
+    }
+
+    // Incremento do índice com base na quantidade de elementos dada para a thread
+    index += args->qntyElements;
+  }
+
+  // Loop de espera da execução de todas as threads
+  for (int i = 0; i < MTHREADS; i++)
+  {
+    if (pthread_join(tid[i], NULL))
+    {
+      printf("ERRO: pthread_join()\n");
+      exit(-2);
+    }
+  }
+
+  // Teste se os resultados são condizentes
+  checkArr();
+
+  // Impressão do vetor resultado
+  printf("\nVetor resultado:\n");
+  for (int i = 0; i < NELEMENTS; i++)
+    printf("%d ", arr[i]);
+
+  return 0;
+}
---
+++ lab01/respostas.md
@@ -0,0 +1,30 @@
+### Atividades
+
+#### Enunciado
+
+Escreva um programa com M _threads_, para elevar ao quadrado cada elemento de um vetor de N elementos do tipo inteiro (para cada elemento do vetor, calcular o novo valor e escrever o resultado na mesma posição do elemento).
+
+#### Roteiro
+
+1. Comece pensando em como dividir a tarefa de elevar ao quadrado todos os elementos de um vetor entre M _threads_. Importante: todas as _threads_ deverão executar a mesma função. Qual(is) argumento(s) deverá(ão) ser passado(s) para a função executada pelas _threads_?
+2. Implemente funções separadas para inicializar e carregar o vetor e verificar se o resultado está correto no final.
+3. Na função `main`, chame a função de inicialização do vetor; crie as _threads_; aguarde o término da execução das _threads_ criadas e chame a função para verificar se os
+   valores finais do vetor estão corretos.
+4. Execute o programa várias vezes e verifique se ele está funcionando corretamente.
+
+#### Respostas
+
+Atividade 1: 
+- 
+
+Atividade 2:
+- 
+
+Atividade 3: 
+- 
+
+Atividade 4: 
+- 
+
+Atividade 5:
+- Implementação do programa está no arquivo `ativ5.c`.
